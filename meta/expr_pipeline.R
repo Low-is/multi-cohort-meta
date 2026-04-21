52165e207456ddb264380b1d9d2e4a03872ac892
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-21 15:36:33 -0500

    Update expr_pipeline.R

--- meta/expr_pipeline.R
+++ meta/expr_pipeline.R
@@ -1,4 +1,20 @@
 library(jsonlite)
 
+# -------------------------
+# LOAD STUDY LISTS
+# -------------------------
 dna_studies <- fromJSON("miner/outputs/dna_gse_ids.json")
 rna_studies <- fromJSON("miner/outputs/rna_gse_ids.json")
+
+# -------------------------
+# RUN PIPELINE
+# -------------------------
+dna_exprs <- generate_exprs_mtx(
+  DNA = TRUE,
+  dna_studies = dna_studies
+)
+
+rna_exprs <- generate_exprs_mtx(
+  RNA = TRUE,
+  rna_studies = rna_studies
+)
