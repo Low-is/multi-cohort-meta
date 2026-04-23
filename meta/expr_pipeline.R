4f154f9605bd2735da497ea6dfa5cd00e7d13dfb
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-23 13:28:44 -0500

    Update expr_pipeline.R

--- meta/expr_pipeline.R
+++ meta/expr_pipeline.R
@@ -58,22 +58,3 @@ message(sprintf("Total RNA datasets processed: %d", length(rna_exprs)))
 message("Saving loaded matrices...")
 saveRDS(dna_exprs, "meta/matrices/dna_matrices.rds")
 saveRDS(rna_exprs, "meta/matrices/rna_matrices.rds")
-
-
-# -------------------------
-# LOADING PDATA
-# -------------------------
-message("🧠 Loading pData...")
-
-dna_pData <- get_pData(studies = dna_studies)
-rna_pData <- get_pData(studies = rna_studies)
-
-message("\n pData successfully loaded!")
-
-
-# -------------------------
-# SAVING RESULTS
-# -------------------------
-message("Saving pData...")
-saveRDS(dna_pData, "meta/pdata/dna_pData.rds")
-saveRDS(rna_pData, "meta/pdata/rna_pData.rds")
