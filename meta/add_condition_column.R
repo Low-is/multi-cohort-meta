7666bd094729005860143b00827ff54fcb26e0f7
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-23 13:04:09 -0500

    Update add_condition_column.R

--- meta/add_condition_column.R
+++ meta/add_condition_column.R
@@ -1,17 +1,17 @@
 # Loading functions
-source("../functions/get_pData.R")
+source("meta/functions/get_pData.R")
 
 # Load config
 library(yaml)
-config <- yaml::read_yaml("../config/config.yaml")
+config <- yaml::read_yaml("meta/config/config.yaml")
 
 # Define search patterns
 case_patterns <- config$case_patterns
 control_patterns <- config$control_patterns
 
 # Loading pData
-dna_pData <- readRDS("../pdata/dna_pData.rds")
-rna_pData <- readRDS("../pdata/rna_pData.rds")
+dna_pData <- readRDS("meta/pdata/dna_pData.rds")
+rna_pData <- readRDS("meta/pdata/rna_pData.rds")
 
 # Apply condition detection + labeling
 dna_pData_cond <- apply_codition_to_list(
@@ -27,10 +27,10 @@ rna_pData_cond <- apply_condition_to_list(
 )
 
 # Save results
-saveRDS(dna_pData_cond, "../pdata/dna_pData_with_condition.rds")
-saveRDS(rna_pData_cond, "../pdata/rna_pData_with_condition.rds")
+saveRDS(dna_pData_cond, "meta/pdata/dna_pData_with_condition.rds")
+saveRDS(rna_pData_cond, "meta/pdata/rna_pData_with_condition.rds")
 # Saving as csv
-write.csv(dna_pData_cond, "../pdata/dna_pData_with_condition.csv")
-write.csv(rna_pData_cond, "../pdata/rna_pData_with_condition.csv")
+write.csv(dna_pData_cond, "meta/pdata/dna_pData_with_condition.csv")
+write.csv(rna_pData_cond, "meta/pdata/rna_pData_with_condition.csv")
 
 message("✅ Condition column successfully added to all studies")
