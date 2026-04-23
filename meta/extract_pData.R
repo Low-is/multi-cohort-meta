d6f312143d423f14a22706ec9043561f9270dbbb
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-23 13:28:52 -0500

    Update extract_pData.R

--- meta/extract_pData.R
+++ meta/extract_pData.R
@@ -1,1 +1,17 @@
+# -------------------------
+# LOADING PDATA
+# -------------------------
+message("🧠 Loading pData...")
 
+dna_pData <- get_pData(studies = dna_studies)
+rna_pData <- get_pData(studies = rna_studies)
+
+message("\n pData successfully loaded!")
+
+
+# -------------------------
+# SAVING RESULTS
+# -------------------------
+message("Saving pData...")
+saveRDS(dna_pData, "meta/pdata/dna_pData.rds")
+saveRDS(rna_pData, "meta/pdata/rna_pData.rds")
