d7486792721a28c8d587159ebe35f9490e0637cf
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-23 11:30:08 -0500

    Update add_condition_column.R

--- meta/add_condition_column.R
+++ meta/add_condition_column.R
@@ -12,3 +12,25 @@ control_patterns <- config$control_patterns
 # Loading pData
 dna_pData <- readRDS("../pdata/dna_pData.rds")
 rna_pData <- readRDS("../pdata/rna_pData.rds")
+
+# Apply condition detection + labeling
+dna_pData_cond <- apply_codition_to_list(
+  dna_pData,
+  case_patterns,
+  control_patterns
+)
+
+rna_pData_cond <- apply_condition_to_list(
+  rna_pData,
+  case_patterns,
+  control_patterns
+)
+
+# Save results
+saveRDS(dna_pData_cond, "../pdata/dna_pData_with_condition.rds")
+saveRDS(rna_pData_cond, "../pdata/rna_pData_with_condition.rds")
+# Saving as csv
+write.csv(dna_pData_cond, "../pdata/dna_pData_with_condition.csv")
+write.csv(rna_pData_cond, "../pdata/rna_pData_with_condition.csv")
+
+message("✅ Condition column successfully added to all studies")
