98b901608138d01e287c1242f44a0bd9914eb3ce
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-23 11:10:49 -0500

    Update add_condition_column.R

--- meta/add_condition_column.R
+++ meta/add_condition_column.R
@@ -1,6 +1,14 @@
 # Loading functions
 source("../functions/get_pData.R")
 
+# Load config
+library(yaml)
+config <- yaml::read_yaml("../config/config.yaml")
+
+# Define search patterns
+case_patterns <- config$case_patterns
+control_patterns <- config$control_patterns
+
 # Loading pData
 dna_pData <- readRDS("../pdata/dna_pData.rds")
 rna_pData <- readRDS("../pdata/rna_pData.rds")
