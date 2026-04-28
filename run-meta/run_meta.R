198549777a63f2563266803eb914ab4cc61e986b
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-28 11:25:05 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -26,12 +26,12 @@ dim(rna_matrices[["GSE106910"]]$expr)
 colnames(rna_matrices[["GSE106910"]]$expr)
 
 # Find common genes across all studies being used for meta-analysis
-message("Searching for common genes...")
-common_genes <- Reduce(intersect, all_matrices)
+#message("Searching for common genes...")
+#common_genes <- Reduce(intersect, all_matrices)
 #common_genes <- find_common_genes(DNA = config$analysis$modalities$DNA,
                                   #RNA = config$analysis$modalities$RNA,
                                   #list_of_dna_mtx = dna_mats,
                                   #list_of_rna_mtx = rna_mats,
                                   #use_DEG = config$analysis$use_DEG
                                  #)
-message(sprintf("%d common genes detected!", length(common_genes)))
+#message(sprintf("%d common genes detected!", length(common_genes)))
