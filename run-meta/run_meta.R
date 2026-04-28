2cddc7b134c36f84976ce2b30a28f5b026fb28b4
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-28 10:53:32 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -22,12 +22,17 @@ message("Matrices loaded!")
 
 # Need to add code that filters matrices to match dimmensions of pData
 
+lapply(dna_matrices, function(mtx) {
+  dim(mtx)
+  head(rownames(mtx))
+})
+
 # Find common genes across all studies being used for meta-analysis
-message("Searching for common genes...")
-common_genes <- find_common_genes(DNA = config$analysis$modalities$DNA,
-                                  RNA = config$analysis$modalities$RNA,
-                                  list_of_dna_mtx = dna_matrices,
-                                  list_of_rna_mtx = rna_matrices,
-                                  use_DEG = config$analysis$use_DEG
-                                 )
-message(sprintf("%d common genes detected!", length(common_genes)))
+#message("Searching for common genes...")
+#common_genes <- find_common_genes(DNA = config$analysis$modalities$DNA,
+                                  #RNA = config$analysis$modalities$RNA,
+                                  #list_of_dna_mtx = dna_matrices,
+                                  #list_of_rna_mtx = rna_matrices,
+                                  #use_DEG = config$analysis$use_DEG
+                                 #)
+#message(sprintf("%d common genes detected!", length(common_genes)))
