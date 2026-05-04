e8b57ed8148161635f2b137d7460fee06981ec25
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-04 15:35:21 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -63,12 +63,13 @@ message(sprintf("%d common genes detected!", length(common_genes)))
 dna_pData <- readRDS("meta/pdata/dna_pData_with_condition.rds")
 rna_pData <- readRDS("meta/pdata/rna_pData_with_condition.rds")
 
-combined_pData <- c(dna_pData, rna_pData)
+lapply(dna_pData, function(x) str(x$condition))
+lapply(rna_pData, function(x) str(x$condition))
 
-lapply(combined_pData, function(x) str(x$condition))
+#combined_pData <- c(dna_pData, rna_pData)
 
-pData <- names(c(dna_matrices, norm_rna_mtxs))
-study <- names(c(dna_matrices, norm_rna_mtxs))
+#pData <- names(c(dna_matrices, norm_rna_mtxs))
+#study <- names(c(dna_matrices, norm_rna_mtxs))
                                      
 #meta_res <- generate_list_for_meta_analysis(
   #DNA = config$analysis$modalities$DNA,
