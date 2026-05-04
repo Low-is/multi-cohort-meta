fafc510e2ac5f489104bf16f47f310ecf509e355
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-04 11:11:09 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -36,16 +36,10 @@ message("Matrices loaded!")
 
 rna_pdata <- readRDS("meta/pdata/rna_pData_with_condition.rds")
 
-#lapply(rna_matrices, function(x) dim(x$expr))
-#lapply(rna_pdata, function(x) dim(x))
-
 
 message("Getting norm RNA counts...")
 norm_rna_mtxs <- get_norm_RNA_counts(rna_matrices, pData = rna_pdata)
 message("Extracted norm RNA counts!")
-
-#lapply(dna_matrices, function(x) dim(x))
-#lapply(norm_rna_mtxs, function(x) dim(x))
 
 
 # Need to add code that filters matrices to match dimmensions of pData
@@ -61,3 +55,17 @@ common_genes <- find_common_genes(DNA = config$analysis$modalities$DNA,
                                   use_DEG = config$analysis$use_DEG
                                  )
 message(sprintf("%d common genes detected!", length(common_genes)))
+
+
+message("Starting meta-analysis...")
+                                     
+meta_res <- generate_list_for_meta_analysis(
+  DNA = config$analysis$modalities$DNA,
+  RNA = config$analysis$modalities$RNA,
+  list_of_dna_mtx = dna_matrices,
+  list_of_rna_mtx = norm_rna_mtxs,
+  study = unlist(c(dna_studies, rna_studies)),
+  common_genes = common_genes
+)
+
+message("Meta-analysis completed!")
