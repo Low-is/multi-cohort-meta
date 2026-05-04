e54c5a58d1f87086a7f9fc83a1254e965a02b337
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-04 15:48:59 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -63,8 +63,8 @@ message(sprintf("%d common genes detected!", length(common_genes)))
 dna_pData <- readRDS("meta/pdata/dna_pData_with_condition.rds")
 rna_pData <- readRDS("meta/pdata/rna_pData_with_condition.rds")
 
-lapply(dna_pData, function(x) dim(x$condition))
-lapply(rna_pData, function(x) dim(x$condition))
+lapply(dna_pData, function(x) length(x$condition))
+lapply(rna_pData, function(x) length(x$condition))
 
 lapply(dna_pData, function(x) sum(is.na(x$condition)))
 lapply(rna_pData, function(x) sum(is.na(x$condition)))
