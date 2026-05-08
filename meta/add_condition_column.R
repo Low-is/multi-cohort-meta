1b306ea5e2c1d3ef0a9a73a94a355443a234f2a8
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-08 14:52:28 -0500

    Update add_condition_column.R

--- meta/add_condition_column.R
+++ meta/add_condition_column.R
@@ -26,8 +26,69 @@ rna_pData_cond <- apply_condition_to_list(
   control_patterns
 )
 
-dna_matrices <- readRDS("meta/matrices/dna_matrices.rds")
-rna
+# Loading expression matrices
+message("Loading expression matrices...")
+dna_matrices <-  readRDS("meta/matrices/dna_matrices.rds")
+dna_matrices <- dna_matrices[!sapply(dna_matrices, is.null)]
+
+# Function to get rid of methylation experiements
+is_meth <- function(genes) {
+  mean(grepl("^cg", genes, ignore.case = TRUE)) > 0.05
+}
+
+dna_matrices <- dna_matrices[!sapply(dna_matrices, function(m) is_meth(rownames(m)))]
+
+rna_matrices <- readRDS("meta/matrices/rna_matrices.rds") # the str is a list of 2: x$expr and x$pData
+rna_matrices <- rna_matrices[!sapply(rna_matrices, is.null)] # Removing NULL single cell datasets
+message("Matrices loaded!")
+
+                                     
+# Need to add code that filters matrices to match dimmensions of pData         
+# Filtering list
+dna_matrices <- dna_matrices[names(dna_pData_cond)] 
+rna_matrices <- rna_matrices[names(rna_pData_cond)]
+
+                                                                                           
+message("Getting norm RNA counts...")
+norm_rna_mtxs <- get_norm_RNA_counts(rna_matrices, pData = rna_pData_cond)
+message("Extracted norm RNA counts!")
+
+                                     
+dna_matrices <- mapply(
+  function(expr, pdata) {
+
+    res <- resolve_matrix_names(
+      expr_colnames = colnames(expr),
+      pdata = pdata
+    )
+
+    expr <- expr[, res$pdata_rows, drop = FALSE]
+    colnames(expr) <- res$matched_gsms
+
+    expr
+  },
+  dna_matrices,
+  dna_pData_cond,
+  SIMPLIFY = FALSE
+)
+
+norm_rna_mtxs <- mapply(
+  function(expr, pdata) {
+
+    res <- resolve_matrix_names(
+      expr_colnames = colnames(expr),
+      pdata = pdata
+    )
+
+    expr <- expr[, res$pdata_rows, drop = FALSE]
+    colnames(expr) <- res$matched_gsms
+
+    expr
+  },
+  norm_rna_mtxs,
+  rna_pData_cond,
+  SIMPLIFY = FALSE
+)
 
 
 # Save results
@@ -35,3 +96,8 @@ saveRDS(dna_pData_cond, "meta/pdata/dna_pData_with_condition.rds")
 saveRDS(rna_pData_cond, "meta/pdata/rna_pData_with_condition.rds")
 
 message("✅ Condition column successfully added to all studies")
+
+saveRDS(dna_matrices, "meta/matrices/dna_matrices.rds")
+saveRDS(norm_rna_mtxs, "meta/matrices/norm_rna_mtxs.rds")
+
+message("✅ Successfully saved updated matrices!")
