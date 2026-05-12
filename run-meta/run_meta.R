8fe5208575caf64665483fb75f8575579c9f7574
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-11 21:06:47 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -49,7 +49,7 @@ dna_matrices <- dna_matrices[names(dna_pData)]
 rna_matrices <- rna_matrices[names(rna_pData)]
 
 #lapply(rna_matrices, function(x) colnames(x$expr))
-rna_pData[["GSE106910"]]
+rna_pData[["GSE106910"]]$title
 
                                                                                                    
 #message("Getting norm RNA counts...")
