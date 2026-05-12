62647c826a186b54350aad5a12ebdaf09cffafde
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-11 21:01:52 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -48,7 +48,8 @@ message("pData loaded!")
 dna_matrices <- dna_matrices[names(dna_pData)] 
 rna_matrices <- rna_matrices[names(rna_pData)]
 
-lapply(rna_matrices, function(x) colnames(x$expr))
+#lapply(rna_matrices, function(x) colnames(x$expr))
+head(rna_pData[["GSE106910"]]
 
                                                                                                    
 #message("Getting norm RNA counts...")
