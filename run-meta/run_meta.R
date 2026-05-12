16a38898ff4b881b09df374383dd3fe120992675
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-11 21:15:31 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -47,9 +47,9 @@ message("pData loaded!")
 # Need to add code that filters matrices to match dimmensions of pData         
 # Filtering list
 dna_matrices <- dna_matrices[names(dna_pData)] 
-rna_matrices <- rna_matrices[names(rna_pData)]
+#rna_matrices <- rna_matrices[names(rna_pData)]
 
-colnames(rna_matrices[["GSE106910"]])
+names(rna_matrices)
 rna_pData[["GSE106910"]]$title
 
                                                                                                    
