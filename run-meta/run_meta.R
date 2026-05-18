af30afb455ec77a80960b2c19a119a31d85ea72d
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-18 09:11:11 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -66,7 +66,7 @@ normalize <- function(x) {
 #normalize(colnames(rna_matrices[["GSE106910"]]$expr))
 
 #rna_pData[["GSE106910"]]$gsm
-colnames(rna_matrices[["GSE106910"]])
+colnames(rna_matrices[["GSE106910"]]$expr)
 
 
                                                                                                    
