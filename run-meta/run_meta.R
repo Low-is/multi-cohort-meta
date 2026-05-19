07c1364fef23941fd8c6ba26587301a466e4abd1
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-05-19 13:19:59 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -90,9 +90,7 @@ valid_studies[is.na(valid_studies)] <- FALSE
 
 dna_matrices <- dna_matrices[valid_studies]
 dna_pData <- dna_pData[valid_studies]
-
-names(c(dna_matrices, rna_matrices))
-                                                                                                 
+                                                                                        
 # Find common genes across all studies being used for meta-analysis
 message("Searching for common genes...")
 common_genes <- find_common_genes(DNA = config$analysis$modalities$DNA,
@@ -105,142 +103,19 @@ message(sprintf("%d common genes detected!", length(common_genes)))
 
 
 message("Starting meta-analysis...")
+                                     
+combined_pData <- c(dna_pData, rna_pData)
 
-meta_list <- list(
-  GSE8586 = list(
-    expr = dna_matrices[["GSE8586"]][common_genes, ],  
-    pheno = dna_pData[["GSE8586"]]$condition, 
-    keys = rownames(dna_matrices[["GSE8586"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(dna_pData[["GSE8586"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ),
-  GSE32472 = list(
-    expr = dna_matrices[["GSE32472"]][common_genes, ],  
-    pheno = dna_pData[["GSE32472"]]$condition, 
-    keys = rownames(dna_matrices[["GSE32472"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(dna_pData[["GSE32472"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ), 
-  GSE188944 = list(
-    expr = dna_matrices[["GSE188944"]][common_genes, ],  
-    pheno = dna_pData[["GSE188944"]]$condition, 
-    keys = rownames(dna_matrices[["GSE188944"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(dna_pData[["GSE188944"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ),
-  GSE225881 = list(
-    expr = dna_matrices[["GSE225881"]][common_genes, ],  
-    pheno = dna_pData[["GSE225881"]]$condition, 
-    keys = rownames(dna_matrices[["GSE225881"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(dna_pData[["GSE225881"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ),
-  GSE108754 = list(
-    expr = dna_matrices[["GSE108754"]][common_genes, ],  
-    pheno = dna_pData[["GSE108754"]]$condition, 
-    keys = rownames(dna_matrices[["GSE108754"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(dna_pData[["GSE108754"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ),
-  GSE108756 = list(
-    expr = dna_matrices[["GSE108756"]][common_genes, ],  
-    pheno = dna_pData[["GSE108756"]]$condition, 
-    keys = rownames(dna_matrices[["GSE108756"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(dna_pData[["GSE108756"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ),
-  GSE219156 = list(
-    expr = rna_matrices[["GSE219156"]][common_genes, ],  
-    pheno = rna_pData[["GSE219156"]]$condition, 
-    keys = rownames(rna_matrices[["GSE219156"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(rna_pData[["GSE219156"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ),
-  GSE106910 = list(
-    expr = rna_matrices[["GSE106910"]][common_genes, ],  
-    pheno = rna_pData[["GSE106910"]]$condition, 
-    keys = rownames(rna_matrices[["GSE106910"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(rna_pData[["GSE106910"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ),
-  GSE125873 = list(
-    expr = rna_matrices[["GSE125873"]][common_genes, ],  
-    pheno = rna_pData[["GSE125873"]]$condition, 
-    keys = rownames(rna_matrices[["GSE125873"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(rna_pData[["GSE125873"]]$condition == "Control", 0, 1), levels = c(0, 1)))
-  ),
-  GSE220135 = list(
-    expr = rna_matrices[["GSE220135"]][common_genes, ],  
-    pheno = rna_pData[["GSE220135"]]$condition, 
-    keys = rownames(rna_matrices[["GSE220135"]][common_genes, ]),  
-    class = factor(as.numeric(ifelse(rna_pData[["GSE220135"]]$condition == "Control", 0, 1), levels = c(0, 1))))
+study <- names(c(dna_matrices, rna_matrices))
+                                     
+meta_res <- generate_list_for_meta_analysis(
+  DNA = config$analysis$modalities$DNA,
+  RNA = config$analysis$modalities$RNA,
+  list_of_dna_mtx = dna_matrices,
+  list_of_rna_mtx = rna_matrices,
+  list_of_pData = combined_pData,
+  study = study,
+  common_genes = common_genes
 )
 
-gse8586_es <- effect.sizes(meta_list$GSE8586)
-gse32472_es <- effect.sizes(meta_list$GSE32472)
-gse188944_es <- effect.sizes(meta_list$GSE188944)
-gse225881_es <- effect.sizes(meta_list$GSE225881)
-gse108754_es <- effect.sizes(meta_list$GSE108754)
-gse108756_es <- effect.sizes(meta_list$GSE108756)
-gse219156_es <- effect.sizes(meta_list$GSE219156)                                    
-gse106910_es <- effect.sizes(meta_list$GSE106910)
-gse125873_es <- effect.sizes(meta_list$GSE125873)
-gse220135_es <- effect.sizes(meta_list$GSE220135)
-
-                                     
-list.of.effects <- list(GSE8586 = gse8586_es,
-                        GSE32472 = gse32472_es,
-                        GSE188944 = gse188944_es,
-                        GSE225881 = gse225881_es,
-                        GSE108754 = gse108754_es,
-                        GSE108756 = gse108756_es,
-                        GSE219156 = gse219156_es,
-                        GSE106910 = gse106910_es,
-                        GSE125873 = gse125873_es,
-                        GSE220135 = gse220135_es)
-
-summary <- combine.effect.sizes(list.of.effects)  
-
-summary$g <- summary$g[!apply(is.na(summary$g) | is.infinite(summary$g), 1, any), ]
-summary$se.g <- summary$se.g[!apply(is.na(summary$se.g) | is.infinite(summary$se.g), 1, any), ]
-g_genes <- rownames(summary$g)
-    
-summary$pooled.estimates$genes <- rownames(summary$pooled.estimates)
-summary$pooled.estimates <- summary$pooled.estimates %>%
-                                     dplyr::filter(genes %in% g_genes)
-    
-summary$pooled.estimates <- summary$pooled.estimates %>%
-                                     dplyr::filter(!apply(is.na(.) | is.infinite(as.matrix(.)), 1, any))
-    
-summary$pooled.estimates <- as.data.frame(lapply(summary$pooled.estimates, function(x) ifelse(x == 0, 1e-200, x)))
-rownames(summary$pooled.estimates) <- g_genes
-
-# Extracting individual estimates                                                 
-g <- summary$g
-se.g <- summary$se.g
-
-# Extracting pooled estimates
-pool    <- summary$pooled.estimates[, "summary"]
-names(pool) <- rownames(g)
-    
-se.pool <- summary$pooled.estimates[, "se.summary"]
-names(se.pool) <- rownames(g)
-
- # Label for forestplots                                               
-x.label <- "Standardized Mean Difference (log2 scale)"
-    
-# Adding FDR corrected p-values
-summary$pooled.estimates <- summary$pooled.estimates %>%
-                                                 dplyr::mutate(
-                                                   FDR = p.adjust(p.value, method = "BH")
-                                                 )
-                                                     
-summary$pooled.estimates
-#combined_pData <- c(dna_pData, rna_pData)
-
-#pData <- names(c(dna_matrices, rna_matrices))
-#study <- names(c(dna_matrices, rna_matrices))
-                                     
-#meta_res <- generate_list_for_meta_analysis(
-  #DNA = config$analysis$modalities$DNA,
-  #RNA = config$analysis$modalities$RNA,
-  #list_of_dna_mtx = dna_matrices,
-  #list_of_rna_mtx = rna_matrices,
-  #list_of_pData = combined_pData,
-  #study = study,
-  #common_genes = common_genes
-#)
-
-#message("Meta-analysis completed!")
+message("Meta-analysis completed!")
