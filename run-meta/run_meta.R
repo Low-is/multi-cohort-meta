bda5aa8225b7a71feffc1a3c2bb4502f05af86be
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-06-18 21:08:35 -0500

    Update run_meta.R

--- run-meta/run_meta.R
+++ run-meta/run_meta.R
@@ -125,7 +125,7 @@ consistent_genes <- meta_res$meta$consistent_genes
 meta_res_df <- meta_res$meta$pooled.estimates
 meta_res_df <- meta_res_df[consistent_genes, ] 
 
-which(meta_res_df$FDR <= 0.05)
+head(meta_res_df$FDR)
                                      
 write.csv(meta_res_df, "run-meta/output/meta_results.csv")
 saveRDS(consistent_genes, "run-meta/output/consistent_genes.rds")
